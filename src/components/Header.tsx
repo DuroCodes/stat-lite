61d887c36fe8d218a8fd6a1f936c8dd58dc07fe9
DuroCodes <davidwright13503@gmail.com>
2024-01-14 17:08:30 -0500

    feat: make Logo link to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 export function Header() {
   return (
     <header className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl grid grid-cols-4 items-center py-4 border-b border-b-zinc-800">
-      <h1 className="text-3xl font-bold">Logo</h1>
+      <Link href='/'>
+        <h1 className="text-3xl font-bold">Logo</h1>
+      </Link>
     </header>
   );
 }
